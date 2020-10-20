2f405dbb489bfe55f1d3e2fdf72c814ec0d2390c
Nunny R <nunnyreyes@gmail.com>
2020-10-20 11:20:14 -0400

    user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
   resources :notes
   resources :memories
-  resources :users
+  resources :users, only: [:create]
+  #the above handles the user signing up 
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post "/users/login", to: "users#login"
+  post "/users", to: "users#create"
+  get "/users/keep_logged_in", to: "users#keep_logged_in"
+
+
+
+
+  #get "/auto_login", to:"users#auto_login"
+  # get "/user_is_authed", to: "users#user_is_authed"
+
+
+
 end
