b9829420bef98af9c1d261f1188d48551fe143c8
ksolomon7 <kalkidan.solomon2@gmail.com>
2020-10-22 20:36:16 -0400

    adding changes for notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,14 @@
 class NotesController < ApplicationController
+    # before_action :authorized, only: [:create]
+
+    def create
+        note=Note.create(notes_params)
+        render json: note
+    end
+
+    private
+
+    def notes_params
+        params.permit(:reflection, :memory_id)
+    end
 end
